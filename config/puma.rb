2b9fcefb6fabb0693750c7f05b8bef73bf92ab61
Efe <knowledgeagare157@gmail.com>
2019-07-18 14:26:28 +0100

    remove binding.pry

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch('PORT') { 3001 }
+port        ENV.fetch('PORT') { 2000}
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -34,3 +34,4 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
